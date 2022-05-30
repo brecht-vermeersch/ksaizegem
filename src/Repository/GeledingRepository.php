1f9556c16ffcfabe3ca0548039d4c8ba38cd9ed6
brecht.vermeersch <brecht.vermeersch@athena-graphics.be>
2022-05-30 11:23:50 +0200

    add default sort

--- src/Repository/GeledingRepository.php
+++ src/Repository/GeledingRepository.php
@@ -11,7 +11,6 @@ use Doctrine\Persistence\ManagerRegistry;
  *
  * @method Geleding|null find($id, $lockMode = null, $lockVersion = null)
  * @method Geleding|null findOneBy(array $criteria, array $orderBy = null)
- * @method Geleding[]    findAll()
  * @method Geleding[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class GeledingRepository extends ServiceEntityRepository
@@ -39,28 +38,8 @@ class GeledingRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Geleding[] Returns an array of Geleding objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('g.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Geleding
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findAll(): array
+    {
+        return $this->findBy([], ['rangschikking' => 'asc']);
+    }
 }
