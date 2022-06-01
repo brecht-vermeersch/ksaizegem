638b767655de90f6569274ecb39046adb7fdb356
Simon Buyck <simon.buyck@student.howest.be>
2022-06-01 17:11:09 +0200

    mobile navigation and interactive buttons

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,10 @@
 import 'normalize.css/normalize.css';
 import './styles/app.scss';
 import './scripts/calendar.js';
+
+{
+    document.addEventListener("DOMContentLoaded", function () {
+      let node = document.querySelector(".preload-transitions");
+      node.classList.remove("preload-transitions");
+    });
+}
